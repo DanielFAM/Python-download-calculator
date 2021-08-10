84349a8f5f8b151069d51fc2cdca5903538b8d61
Daniel Andrade <danfe_1999@outlook.com>
2021-08-09 22:06:09 -0500

    construction of menu

---
+++ calculator.py
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    print('<-------------------------Time download calculator------------------------------>')
+    election = input('Select the size of the file: \n1.KB\n2.MB\n3.GB\n')
+
+    if election == 1:
+        size = input('Input de size of file in KB: ')
+    elif election == 2:
+        size = input('Input de size of file in MB: ')
+    elif election == 3:
+        size = input('Input de size of file in GB: ') 
+
+    
+
+   
