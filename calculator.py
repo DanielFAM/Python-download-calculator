d58093bb1d6373ce68cc425c1ae0d5715c754fb5
Daniel Andrade <danfe_1999@outlook.com>
2021-08-09 22:42:41 -0500

    Final release v1.0

--- calculator.py
+++ calculator.py
@@ -1,6 +1,12 @@
 def time_generator (size,speed):
     return size / speed;
 
+def time_download_calculator(seconds):
+    hours = int(seconds / 3600)
+    minutes = int((seconds-hours*3600)/60)
+    seconds = int(seconds-(hours*3600 + minutes*60))
+    print ('Estimated time for download: ', hours,'Hours ',minutes,'Minutes ',seconds, 'Seconds ' )
+
 
 if __name__ == '__main__':
     print('<-------------------------Time download calculator------------------------------>')
@@ -12,17 +18,19 @@ if __name__ == '__main__':
         size = int(input('Input the size of file in KiloBytes: '))
         size = size * 0.008
         result = time_generator (size, speed)
+        time_download_calculator(result)
 
     elif election == 2:
         size = int(input('Input the size of file in MegaBytes: '))
         size = size * 8
         result = time_generator (size, speed)
+        time_download_calculator(result)
 
     elif election == 3:
         size = int(input('Input the size of file in GigaBytes: '))
         size = size * 8000
         result = time_generator (size, speed)
-        print (result)
+        time_download_calculator(result)
     
     else:
         print('Wrong election, please try again....\n')
