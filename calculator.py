9190029828fd62aeaddd730f508538a7bfa28957
Daniel Andrade <danfe_1999@outlook.com>
2021-08-09 22:24:46 -0500

    parsing of data types and treatment of erroneous choices has been added

--- calculator.py
+++ calculator.py
@@ -5,23 +5,27 @@ def time_generator (size,speed):
 if __name__ == '__main__':
     print('<-------------------------Time download calculator------------------------------>')
     
-    speed = input('Input the speed of your internet: ')
-    election = input('Select the size of the file: \n1.KB\n2.MB\n3.GB\n')
+    speed = int(input('Input the speed of your internet in Megabits for second: '))
+    election = int(input('Select the size of the file: \n1.KB\n2.MB\n3.GB\n'))
 
     if election == 1:
-        size = input('Input the size of file in KiloBytes: ')
+        size = int(input('Input the size of file in KiloBytes: '))
         size = size * 0.008
         result = time_generator (size, speed)
 
     elif election == 2:
-        size = input('Input the size of file in MegaBytes: ')
+        size = int(input('Input the size of file in MegaBytes: '))
         size = size * 8
         result = time_generator (size, speed)
 
     elif election == 3:
-        size = input('Input the size of file in GigaBytes: ')
+        size = int(input('Input the size of file in GigaBytes: '))
         size = size * 8000
         result = time_generator (size, speed)
+        print (result)
+    
+    else:
+        print('Wrong election, please try again....\n')
 
     
 
